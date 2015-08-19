455363b375155b190e39399f6e15382c7315ff87
Vivek Dinesh <vivek@wisptools.com>
2015-08-19 11:42:12 +0530

    More template changes and JavaScript to make them functional.

--- client/templates/archivetools/firstarchive.js
+++ client/templates/archivetools/firstarchive.js
@@ -23,5 +23,10 @@ Template.fileSelectWidgetServiceTab.events({
             $('#firstarchive .myarchivecontent').eq(0).slideUp();
             $('#firstarchive .myarchivesummary').eq(0).removeClass('expanded');
         }
+        $('.servicetabitem').removeClass('selected');
+        $(e.currentTarget).addClass('selected');
+        var service = $(e.currentTarget).find('p').eq(0).html();
+        $('.servicecontentitem').hide();
+        $('#service'+service.replace(' ', '').toLowerCase()).show();
     }
 });
